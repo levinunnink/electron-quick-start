e4ca6889fbb6c3dd8e372b0c6baa2937cf619bb2
IEUser <levi@droplr.com>
2018-06-25 11:11:28 -0700

    Demonstrates the broken desktopCapturer api on Windows 7

--- renderer.js
+++ renderer.js
@@ -1,3 +1,12 @@
 // This file is required by the index.html file and will
 // be executed in the renderer process for that window.
 // All of the Node.js APIs are available in this process.
+
+const {desktopCapturer} = require('electron')
+  
+desktopCapturer.getSources({types: ['window', 'screen']}, (error, sources) => {
+  if (error) throw error
+  for (let i = 0; i < sources.length; ++i) {
+    console.log('Source', sources[i])
+  }
+})
